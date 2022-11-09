8fe8b28c5483baff17b15ec28adc10478c4ca959
leoleducq <73238856+leoleducq@users.noreply.github.com>
2022-11-09 16:07:31 +0100

    Create Welcome!.rs

---
+++ Rust/8/Welcome!.rs
@@ -0,0 +1,22 @@
+fn greet(language: &str) -> &str {
+    match language {
+        "english" => "Welcome",
+        "czech" => "Vitejte",
+        "danish" => "Velkomst",
+        "dutch" => "Welkom",
+        "estonian" => "Tere tulemast",
+        "finnish" => "Tervetuloa",
+        "flemish" => "Welgekomen",
+        "french" => "Bienvenue",
+        "german" => "Willkommen",
+        "irish" => "Failte",
+        "italian" => "Benvenuto",
+        "latvian" => "Gaidits",
+        "lithuanian" => "Laukiamas",
+        "polish" => "Witamy",
+        "spanish" => "Bienvenido",
+        "swedish" => "Valkommen",
+        "welsh" => "Croeso",
+        _ => "Welcome",
+    }
+}
