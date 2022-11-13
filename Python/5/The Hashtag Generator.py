1d5e8187281a473d36a4b9c78a2e6aab7cdd2a74
leoleducq <73238856+leoleducq@users.noreply.github.com>
2022-11-13 01:41:20 +0100

    Clever solution

--- Python/5/The Hashtag Generator.py
+++ Python/5/The Hashtag Generator.py
@@ -1,5 +1,4 @@
 def generate_hashtag(s):
     if len(s) == 0 or len(s) > 140:
         return False
-    else:
-        return '#' + ''.join([word.capitalize() for word in s.split()])
+    return '#' + ''.join([word.capitalize() for word in s.split()])
