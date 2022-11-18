8972490fc60c63e703fc33754fc376ef44a99e92
Léo LEDUCQ <73238856+leoleducq@users.noreply.github.com>
2022-11-18 10:14:26 +0100

    Update Evaluate mathematical expression.py

--- Python/2/Evaluate mathematical expression.py
+++ Python/2/Evaluate mathematical expression.py
@@ -5,27 +5,27 @@ def calc(expression):
     if re.search(r"[\+\-\*\/]{2,}",expression):
         return None
     # Cherche un signe d'opération
-    if re.search(r"[\+\-\*\/]",expression):
-        while "*" in expression or "/" in expression:
-            calculs = re.search(r"(\d+(?:\.\d+)?)[\*\/](\d+(?:\.\d+)?)",expression)
-            # Récupère les valeurs avant et après le signe d'opération
-            values = re.findall(r"(\d+(?:\.\d+)?)",calculs.group())
-            # Récupère le signe d'opération
-            operator = re.search(r"[\+\-\*\/]",calculs.group()).group()
-            # Fait le calcul
-            sum = calcul(values[0],values[1],operator)
-            # Remplace value1, value2 et operator par le resultat de l'opération
-            # print("{} : {} {} {} = {}".format(expression, values[0],operator,values[1],sum))
-            expression = replace(expression, values[0], values[1], operator)
-
-        while "+" in expression or "-" in expression:
-            calculs = re.search(r"(\d+(?:\.\d+)?)[\+\-](\d+(?:\.\d+)?)",expression)
-            values = re.findall(r"(\d+(?:\.\d+)?)",calculs.group())
-            operator = re.search(r"[\+\-\*\/]",calculs.group()).group()
-            sum = calcul(values[0],values[1],operator)
-            # print("{} : {} {} {} = {}".format(expression,values[0],operator,values[1],sum))
-            expression = replace(expression, values[0], values[1], operator)
+    if re.search(r"[\*\/]",expression):
+        sum, expression = determinate("*","/",expression,"(\d+(?:\.\d+)?)[\*\/](\d+(?:\.\d+)?)")
+    if re.search(r"[\+\-]",expression):
+        sum, expression = determinate("+","-",expression, "(\d+(?:\.\d+)?)[\+\-](\d+(?:\.\d+)?)")
+    try:
         return sum
+    except:
+        return re.search(r"(\d+(?:\.\d+)?)",expression).group()
+
+def determinate(sign1,sign2, expression,regex_calcul):
+    while sign1 in expression or sign2 in expression:
+        calculs = re.search(rf"{regex_calcul}",expression).group()
+        # Récupère les valeurs avant et après le signe d'opération
+        values = re.findall(r"(\d+(?:\.\d+)?)",calculs)
+        # Récupère le signe d'opération
+        operator = re.search(r"[\+\-\*\/]",calculs).group()
+        # Fait le calcul
+        sum = calcul(values[0],values[1],operator)
+        # Remplace value1, value2 et operator par le resultat de l'opération
+        expression = replace(expression, values[0], values[1], operator)
+    return sum,expression
 
 def calcul(value1, value2, operator):
     # Change les valeurs en float
@@ -35,8 +35,10 @@ def calcul(value1, value2, operator):
 
 # Fonction qui remplace value1, value2 et operator par le resultat de l'opération
 def replace(expression, value1, value2, operator):
-    return expression.replace(str(value1) + operator + str(value2), str(calcul(value1, value2, operator)))  
+    return expression.replace(str(value1) + operator + str(value2), str(calcul(value1, value2, operator)))
 
+print(calc("10- 2- -5"))
+print(calc("(((10)))"))
 print(calc("1 -- 1"))
 print(calc("1 + 1"))
 print(calc("2 - 1"))
